4ea897e036b736ed608aa32bb101d01c650dc08c
vishaltiwari021 <vishaltiwarivt021@gmail.com>
2026-03-20 11:48:34 +0530

    learn about middlewares.

--- Day10Nodejs/app.js
+++ Day10Nodejs/app.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const userRoutes = require("./router/userrouters.js");
+const dotenv = require("dotenv");
 
 
+dotenv.config();
 const app = express();
+const JWT = process.env.JWT;
+
+const coustomMiddleware =(req,res,next)=>{
+    if(req.query.skip == "true"){
+        next();
+    }else{
+        res.send("Not Authorised");
+    }
+}
+// const authentication = (req,res,next)=>{
+//     if(req.headers == JWT){
+
+//     }
+// }
+
+app.use(coustomMiddleware);
+
 app.use(express.json())
 app.use('/get',userRoutes);
 
--- Day10Nodejs/index.js
+++ Day10Nodejs/index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT;
 
 
 
+
 app.listen(PORT,()=>{
     console.log(`server is running on port: ${PORT}`);
     
